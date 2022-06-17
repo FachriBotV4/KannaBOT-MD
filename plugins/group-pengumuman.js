8c457c52bb09c66e088b6cfbe9027cced684656a
Fachri Botz <94295494+Ipulpachri@users.noreply.github.com>
2022-06-17 20:05:42 +0700

    Update group-pengumuman.js

--- plugins/group-pengumuman.js
+++ plugins/group-pengumuman.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text, participants }) => {
   )
   await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
 }
-handler.help = ['pengumuman', 'announce', 'hidetag'].map(v => v + ' [teks]')
+handler.help = ['pengumuman', 'announce'].map(v => v + ' [teks]')
 handler.tags = ['group']
 handler.command = /^(pengumuman|announce|hiddentag|hidetag)$/i
 
