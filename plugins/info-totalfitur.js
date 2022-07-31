2b4da96a7005694f283d299172131261e1a0755c
Hm <110269965+FachriBotV4@users.noreply.github.com>
2022-07-31 07:58:05 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -1,9 +1,11 @@
 import fs from 'fs'
+import fetch from 'node-fetch'
 let handler = async (m, { conn, args, command }) => {
+let fkontak = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(m.chat ? { remoteJid: `status@broadcast` } : {}) }, message: { 'contactMessage': { 'displayName': wm, 'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:XL;${wm},;;;\nFN:${wm},\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabell:Ponsel\nEND:VCARD`, 'jpegThumbnail': fs.readFileSync('./IMG-20220707-WA0022.jpg'), thumbnail: fs.readFileSync('./IMG-20220707-WA0022.jpg'),sendEphemeral: true}}}
 let totalf = Object.values(global.plugins).filter(
     (v) => v.help && v.tags
   ).length;
- await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], m, {
+ await conn.sendButton(m.chat, `Total Fitur Bot Saat ini: ${totalf}\n`,wm + '\n\n' + botdate, giflogo, [['MENU','.menu']], fkontak, {
 contextInfo: { externalAdReply :{
                         mediaUrl: '',
                         mediaType: 2,
