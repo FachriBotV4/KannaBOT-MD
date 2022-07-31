51eb8353f4c146bf9ad65922e1b75e27f931564f
Hm <110269965+FachriBotV4@users.noreply.github.com>
2022-07-31 07:53:45 +0700

    Update info-totalfitur.js

--- plugins/info-totalfitur.js
+++ plugins/info-totalfitur.js
@@ -10,7 +10,7 @@ contextInfo: { externalAdReply :{
                         description: 'anu',
                         title: bottime,
                         body: 'Total Cintaku Padamu',          previewType: 0,
-                        thumbnail: fs.readFileSync("./thumbnail.jpg"),
+                        thumbnail: fs.readFileSync("./94295494.jpeg"),
                         sourceUrl: sig
                       }}
 })
